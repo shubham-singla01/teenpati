2915152a7a38a28eea85a1de4e3f553f478dd2ad
Shubham Singla <s.singla1996@gmail.com>
2017-05-15 21:06:14 +0530

    Initial Commit
    Without node_modules

---
+++ config.js
@@ -0,0 +1,13 @@
+var config={
+	database:{
+		mode:'remote', // can be local or remote
+		host:'ds064299.mlab.com',
+		port:64299,
+		dbname:'teenpati',
+		username:'milind',
+		password:'Asdf'
+	}
+};
+
+
+module.exports = config;
